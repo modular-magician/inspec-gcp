df0490954feda86ec394acdd961095c7716c89c6
Chris Stephens <chrisst@google.com>
2019-12-11 19:03:58 +0000

    Allow for no-release-note to be used in relesenote block
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- libraries/google/compute/property/urlmap_path_matchers.rb
+++ libraries/google/compute/property/urlmap_path_matchers.rb
@@ -16,6 +16,7 @@
 require 'google/compute/property/urlmap_path_matchers_header_action'
 require 'google/compute/property/urlmap_path_matchers_header_action_request_headers_to_add'
 require 'google/compute/property/urlmap_path_matchers_header_action_response_headers_to_add'
+require 'google/compute/property/urlmap_path_matchers_path_rules'
 module GoogleInSpec
   module Compute
     module Property
@@ -28,6 +29,8 @@ module GoogleInSpec
 
         attr_reader :name
 
+        attr_reader :path_rules
+
         def initialize(args = nil, parent_identifier = nil)
           return if args.nil?
           @parent_identifier = parent_identifier
@@ -35,6 +38,7 @@ module GoogleInSpec
           @description = args['description']
           @header_action = GoogleInSpec::Compute::Property::UrlMapPathMatchersHeaderAction.new(args['headerAction'], to_s)
           @name = args['name']
+          @path_rules = GoogleInSpec::Compute::Property::UrlMapPathMatchersPathRulesArray.parse(args['pathRules'], to_s)
         end
 
         def to_s
