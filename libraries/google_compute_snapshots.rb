bbcb425b12b6448f9ebdf1afe17b75a29a0bf828
Modular Magician <magic-modules@google.com>
2020-01-17 19:06:05 +0000

    New generated code for MM PR 2986.

--- libraries/google_compute_snapshots.rb
+++ libraries/google_compute_snapshots.rb
@@ -98,12 +98,8 @@ class ComputeSnapshots < GcpResourceBase
 
   private
 
-  def product_url(beta = false)
-    if beta
-      'https://www.googleapis.com/compute/beta/'
-    else
-      'https://www.googleapis.com/compute/v1/'
-    end
+  def product_url(_ = nil)
+    'https://www.googleapis.com/compute/v1/'
   end
 
   def resource_base_url
