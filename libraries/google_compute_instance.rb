a7d3990c141e81ee632bc1e239e903bdbd80465e
Modular Magician <magic-modules@google.com>
2020-01-22 22:02:33 +0000

    Revert 2955 as it broke tests. (#3008)
    Merged PR #3008.

--- libraries/google_compute_instance.rb
+++ libraries/google_compute_instance.rb
@@ -231,12 +231,8 @@ class ComputeInstance < GcpResourceBase
 
   private
 
-  def product_url(beta = false)
-    if beta
-      'https://www.googleapis.com/compute/beta/'
-    else
-      'https://www.googleapis.com/compute/v1/'
-    end
+  def product_url(_ = nil)
+    'https://www.googleapis.com/compute/v1/'
   end
 
   def resource_base_url
