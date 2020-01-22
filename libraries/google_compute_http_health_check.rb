9a87ee3817dce16ba2b1a589fdb9ff4f1b74adbd
Modular Magician <magic-modules@google.com>
2020-01-22 21:05:46 +0000

    New generated code for MM PR 3001.

--- libraries/google_compute_http_health_check.rb
+++ libraries/google_compute_http_health_check.rb
@@ -70,12 +70,8 @@ class ComputeHttpHealthCheck < GcpResourceBase
 
   private
 
-  def product_url(beta = false)
-    if beta
-      'https://www.googleapis.com/compute/beta/'
-    else
-      'https://www.googleapis.com/compute/v1/'
-    end
+  def product_url(_ = nil)
+    'https://www.googleapis.com/compute/v1/'
   end
 
   def resource_base_url
